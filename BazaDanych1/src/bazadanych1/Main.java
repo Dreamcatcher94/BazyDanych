8a596f3a71979bc881adb8e8b92e093ac2e56a51
Dreamcatcher94 <b1965501@trbvn.com>
2016-03-15 05:12:15 +0100

    commit
    commit

---
+++ BazaDanych1/src/bazadanych1/Main.java
@@ -0,0 +1,30 @@
+package bazadanych1;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * Created by Marta Pawlik on 21.02.2016.
+ */
+
+public class Main {
+
+
+    public static void main(String[] args) throws SQLException {
+        String query = "";
+        DataBaseConnect dbc = new DataBaseConnect();
+        Scanner scanner = new Scanner(System.in);
+        dbc.PobierzInfo("select * from employees");
+       
+       System.out.println("Nacisnij 0 aby przejsc do wprowadzania zmian w DB");
+       if(scanner.nextInt() == 0){
+           System.out.println("Wprowadź komendę");
+           scanner.nextLine();
+           query = scanner.nextLine();
+           dbc.zaktualizujDB(query);
+           
+       }
+       
+    }
+    
+}
